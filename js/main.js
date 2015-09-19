435207348e3ddbf98f20d86e6f9575b375ffeefe
Erik Woitschig <erik@sourcloud.com>
2015-09-19 13:22:54 +0200

    GA added to repo

--- js/main.js
+++ js/main.js
@@ -64,3 +64,16 @@ window.addEventListener('shake', shakeEventDidOccur, false);
 		$('#maincontent').append('<span class="c64pma px16"><br/><br/>GDG Berlin<br/>DevFest 2013<br/><br/>bnz<br/><br/>bit.ly/1aUJgxU</span>');
 		}
 });
+
+/**
+* Google Analytics
+* bnz-power.com
+*/
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-59815538-1', 'auto');
+ga('send', 'pageview');
